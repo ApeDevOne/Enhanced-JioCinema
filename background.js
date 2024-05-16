483051ab9adabceeb30beb20db5929d5f22c7bf6
RocketBoy <91538975+ApeDevOne@users.noreply.github.com>
2024-05-16 14:12:42 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-// background.js
 function changeSubtitleSize(size) {
   const textContainer = document.querySelector('.shaka-text-container');
   if (textContainer) {
-    const textSpans = textContainer.querySelectorAll('.shaka-text-wrapper');
+    const textSpans = textContainer.querySelectorAll('.shaka-text-region span span');
     textSpans.forEach(span => {
       span.style.fontSize = `${size}px`;
     });
