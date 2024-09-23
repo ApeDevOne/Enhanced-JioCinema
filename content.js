90f242940fe009c249e1ed4e39fb0c882a3ea7cb
RocketBoy <91538975+ApeDevOne@users.noreply.github.com>
2024-09-23 22:45:07 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,27 +1,46 @@
 function changeSubtitleStyle(size, font, opacity) {
-  const textContainer = document.querySelector('.shaka-text-container');
-  if (textContainer) {
-    const observer = new MutationObserver(() => {
-      const textRegions = document.querySelectorAll('.shaka-text-region');
-      textRegions.forEach(region => {
-        const textSpans = region.querySelectorAll('span span');
-        textSpans.forEach(span => {
-          span.classList.add('custom-subtitle-style');
-        });
+  const observer = new MutationObserver(() => {
+    const textContainers = document.querySelectorAll('div[class^="mui-style-"][class$="-subtitlesContainer"]');
+    textContainers.forEach(container => {
+      const textSpans = container.querySelectorAll('span');
+      textSpans.forEach(span => {
+        span.classList.add('custom-subtitle-style');
       });
-      const style = document.createElement('style');
-      style.innerHTML = `
-        .shaka-text-region span span.custom-subtitle-style {
-          font-size: ${size}px !important;
-          font-family: ${font} !important;
-          background-color: rgba(0, 0, 0, ${opacity}) !important;
-        }
-      `;
-      document.head.appendChild(style);
     });
-    observer.observe(textContainer, {childList: true, subtree: true});
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+      div[class^="mui-style-"][class$="-subtitlesContainer"] span.custom-subtitle-style {
+        font-size: ${size}px !important;
+        font-family: ${font} !important;
+        background-color: rgba(0, 0, 0, ${opacity}) !important;
+      }
+    `;
+    document.head.appendChild(style);
+  });
+
+  const subtitleContainer = document.querySelector('#subtitle-1');
+  if (subtitleContainer) {
+    observer.observe(subtitleContainer, { childList: true, subtree: true });
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
